4c9fe2f6325a5c3d38c8ed840e0e4b4782da3ca8
Oliver Klemenz <oklemenz@web.de>
2021-01-13 08:47:33 +0100

    update cache, remove apache stuff, replaced by http-server

--- local/ln-sites-ws.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ln -s ~/Documents/GitHub/Website ~/Sites/ws
--- local/start-node.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-open -a Google\ Chrome http://127.0.0.1:8080
-http-server /Users/oklemenz/Documents/Github/Website
-
-
