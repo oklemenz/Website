dfeb8739d73ec17b6364b7c4213852a05582c1aa
Oliver Klemenz <oklemenz@web.de>
2022-11-24 16:19:27 +0100

    Update, Fixes

--- js/main.js
+++ js/main.js
@@ -1,111 +1,102 @@
 /*
- * Website Main v1.2
- * Copyright 2013-2017, Oliver Klemenz, https://www.oklemenz.de
+ * Website Main v1.3
+ * Copyright 2013-2022, Oliver Klemenz, https://www.oklemenz.de
  */
 (function ($, $window) {
-	    
-    $(this).okLanguage({
-        langCode: $(this).okGetLangCode()
+
+  $(this).okLanguage({
+    langCode: $(this).okGetLangCode()
+  });
+
+  var okScrollImageSize = [];
+  okScrollImageSize["desktop"] = {
+    width: 1600,
+    height: 1060
+  };
+  okScrollImageSize["tablet"] = {
+    width: 1024,
+    height: 678
+  };
+  okScrollImageSize["phone"] = {
+    width: 568,
+    height: 376
+  };
+
+  var okConfig = {
+    scrollImageWidth: okScrollImageSize[$(this).okGetDeviceType()].width,
+    scrollImageHeight: okScrollImageSize[$(this).okGetDeviceType()].height,
+    scrollDynamicContent: !$(this).okIsMobileDevice() && !$(this).okIsIE()
+  };
+
+  $(this).okImageRetina();
+  $(this).okNowDate();
+  $(this).okNowYearsSince();
+  $(this).okDropDownMenu();
+  $(this).okScrollZoomIn();
+
+  var touchCarousel;
+
+  if (okConfig.scrollDynamicContent) {
+    function update(event) {
+      $(this).okResizeAutoSize();
+      $(this).okScrollSizeHeight();
+      $(this).okScrollZoomIn();
+      $(this).okScrollSpeed();
+      $(this).okScrollOpacity();
+      $(this).okScrollClass();
+    }
+
+    var parallexScrolling = $(this).okParallexScroll({
+      speed: 0.25,
+      scrollImageWidth: okConfig.scrollImageWidth,
+      scrollImageHeight: okConfig.scrollImageHeight
     });
 
-    var okScrollImageSize = [];
-    okScrollImageSize["desktop"] = {
-        width: 1600,
-        height: 1060
-    };
-    okScrollImageSize["tablet"] = {
-        width: 1024,
-        height: 678
-    };
-    okScrollImageSize["phone"] = {
-        width: 568,
-        height: 376
-    };
+    $window.load(function (event) {
+      update();
+      touchCarousel = $(this).okCarousel();
+      parallexScrolling.init();
+      parallexScrolling.update();
+    });
 
-    var okConfig = {
-        scrollImageWidth: okScrollImageSize[$(this).okGetDeviceType()].width,
-        scrollImageHeight: okScrollImageSize[$(this).okGetDeviceType()].height,
-        scrollDynamicContent: !$(this).okIsMobileDevice() && !$(this).okIsIE()
-    };
+    $window.resize(function (event) {
+      update();
+      parallexScrolling.init();
+      parallexScrolling.refresh();
+    });
 
-    $(this).okImageRetina();
-    $(this).okNowDate();
-    $(this).okNowYearsSince();
-    $(this).okDropDownMenu();
-	$(this).okScrollZoomIn();
-	
-	var touchCarousel;
-	
-    if (okConfig.scrollDynamicContent) {
-        function update(event) {
-            $(this).okResizeAutoSize();
-            $(this).okScrollSizeHeight();
-            $(this).okScrollZoomIn();
-            $(this).okScrollSpeed();
-            $(this).okScrollOpacity();
-            $(this).okScrollClass();
-        }
-
-        var parallexScrolling = $(this).okParallexScroll({
-            speed: 0.25,
-            scrollImageWidth: okConfig.scrollImageWidth,
-            scrollImageHeight: okConfig.scrollImageHeight
+    $window.scroll(function (event) {
+      if (okConfig.scrollDynamicContent) {
+        update();
+        parallexScrolling.refresh({
+          scrollPos: $(event).okScrollPos()
         });
+      }
+    });
+  } else {
+    $(this).okNoScrollDyanmicContent();
 
-        $window.load(function (event) {
-            update();
-            touchCarousel = $(this).okCarousel();
-            parallexScrolling.init();
-            parallexScrolling.update();
-        });
+    $window.load(function (event) {
+      touchCarousel = $(this).okCarousel();
+      $(this).okNoScrollDyanmicContentUpdate();
+    });
 
-        $window.resize(function (event) {
-            update();
-            parallexScrolling.init();
-            parallexScrolling.refresh();
-        });
+    $window.resize(function (event) {
+      $(this).okNoScrollDyanmicContentUpdate();
+    });
+  }
 
-        $window.scroll(function (event) {
-            if (okConfig.scrollDynamicContent) {
-                update();
-                parallexScrolling.refresh({
-                    scrollPos: $(event).okScrollPos()
-                });
-            }
-        });
-    } else {
-        $(this).okNoScrollDyanmicContent();
-
-        $window.load(function (event) {
-            touchCarousel = $(this).okCarousel();
-            $(this).okNoScrollDyanmicContentUpdate();
-        });
-
-        $window.resize(function (event) {
-            $(this).okNoScrollDyanmicContentUpdate();
-        });
+  // Orientation change
+  $(window).bind("onorientationchange" in window ? "orientationchange" : "resize", function () {
+    if (touchCarousel) {
+      //touchCarousel.data("touchCarousel");
     }
-    
-    // Orientation change
-	$(window).bind("onorientationchange" in window ? "orientationchange" : "resize", function() {		
-		if (touchCarousel) {
-			//touchCarousel.data("touchCarousel");
-		}
-	});
-        
-	// Chrome reflection animation fix
-	$(".reflection").one("webkitAnimationEnd", function(e) {
-    	var reflection = $(".reflection");
-		reflection.removeClass("reflection");
-    	setTimeout(function() {
-    		reflection.addClass("reflection");
-    	}, 0);
-	});
-	
-	// Status bar fix in standalone mode
-	if (!!window.navigator.standalone) {
-		$("#header").addClass("header_status_bar");
-		$(".header_title_status_bar").addClass("header_title_status_bar_show");
-	}
+  });
+
+  // Status bar fix in standalone mode
+  if (!!window.navigator.standalone) {
+    $("#header").addClass("header_status_bar");
+    $(".header_title_status_bar").addClass("header_title_status_bar_show");
+  }
 
 }(jQuery, $(window)));
